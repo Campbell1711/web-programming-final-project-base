0764f983809c2bddd8ae844a190f6eb2a85d7c69
jurgen <tigeryhw@gmail.com>
2021-04-06 10:05:16 +0800

    jurgen second try

--- public/documentUpload.js
+++ public/documentUpload.js
@@ -2,4 +2,12 @@ let inputTitle = document.getElementById('textTitle')
 let inputAuthor = document.getElementById('author')
 let inputTags = document.getElementById('tags')
 
-let 
+let form = document.getElementById('submitForm')
+
+let titleValid = false;
+let authorValid = false;
+let tagsValid = false;
+let textValid = false;
+
+fnameInput.addEventListener('input', checkName);
+lnameInput.addEventListener('input', checkName);
