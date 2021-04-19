31040680d838322e52d2c035c685f72a089f6476
Yuyang Liu <yuyangliu.0728@gmail.com>
2021-04-19 21:22:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -247,7 +247,7 @@ express()
 .get('/db', async(req, res) => {
     try {
         const client = await pool.connect();
-        const result = await client.query('SELECT * FROM order_table');
+        const result = await client.query('SELECT * FROM non_content_table');
         const results = { 'results': (result) ? result.rows : null };
         res.render('pages/db', results);
         client.release();
