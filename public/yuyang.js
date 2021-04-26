ac86c96bae246d163b1b9106024487d9b6afa1ce
Ryan Campbell <campbellryan19@gmail.com>
2021-04-26 16:16:30 -0400

    Fixing document upload

--- public/yuyang.js
+++ public/yuyang.js
@@ -1,26 +1,24 @@
-let inputTitle = document.getElementById('textTitle')
-let inputAuthor = document.getElementById('author')
-let inputTags = document.getElementById('tags')
-let metaWarning = document.getElementById('metaWarning')
+let inputScene = document.getElementById('textScene');
+let inputPlay = document.getElementById('play');
+let metaWarning = document.getElementById('metaWarning');
 
-let textInput = document.getElementById('text')
-let textWarning = document.getElementById('textWarning')
+let textInput = document.getElementById('text');
+let textWarning = document.getElementById('textWarning');
 
-let form = document.getElementById('submitForm')
+let form = document.getElementById('submitForm');
 
 let metaValid = false;
 let textValid = false;
 
-inputTitle.addEventListener('input', checkMeta);
-inputAuthor.addEventListener('input', checkMeta);
-inputTags.addEventListener('input', checkMeta);
+inputScene.addEventListener('input', checkMeta);
+inputPlay.addEventListener('input', checkMeta);
 
 function checkMeta() {
-    metaValid = inputTitle.value && (inputAuthor.value && inputTags.value)
+    metaValid = inputScene.value && inputPlay.value;
     if (!metaValid) {
-        metaWarning.style.display = 'block'
+        metaWarning.style.display = 'block';
     } else {
-        metaWarning.style.display = 'none'
+        metaWarning.style.display = 'none';
     }
 }
 
@@ -29,24 +27,20 @@ textInput.addEventListener('input', checkText);
 function checkText() {
     textValid = textInput.value ? true : false;
     if (!textValid) {
-        textWarning.style.display = 'block'
+        textWarning.style.display = 'block';
     } else {
-        textWarning.style.display = 'none'
+        textWarning.style.display = 'none';
     }
 }
 
 form.addEventListener('submit', function (e) {
-    if (!(metaValid && textValid)) {
-        e.preventDefault();
-    }
-    else {
-        let formData = new FormData(form)
+    if (metaValid && textValid) {
         let xhr = new XMLHttpRequest();
         xhr.addEventListener('load', function (e) {
-            console.log('success!')
+            console.log('success!');
         })
         xhr.open('POST', '/yuyang');
-        xhr.send(formData)
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send(`textScene=${inputScene.value}&play=${inputPlay.value}&text=${textInput.value}`);
     }
 })
-
